244b61d8bc489d140633c23f7c312dbe4fa05236
azizback <azizdzheendkov@gmail.com>
2022-05-07 23:31:16 +0300

    initialize objects

---
+++ src/constants/footerItem.ts
@@ -0,0 +1,29 @@
+import SonyLogo from "../assets/images/sonyLogo.svg";
+import UbisoftLogo from "../assets/images/ubisoftLogo.svg";
+import ValveLogo from "../assets/images/valveLogo.svg";
+import NintendoLogo from "../assets/images/nintendoLogo.svg";
+
+const footerItem = [
+  {
+    linkRef: "https://www.sony.com/",
+    imageLink: SonyLogo,
+    alt: "Sony Logo",
+  },
+  {
+    linkRef: "https://www.nintendo.com/",
+    imageLink: NintendoLogo,
+    alt: "Nintendo Logo",
+  },
+  {
+    linkRef: "https://www.ubisoft.com",
+    imageLink: UbisoftLogo,
+    alt: "Ubisoft Logo",
+  },
+  {
+    linkRef: "https://www.valvesoftware.com",
+    imageLink: ValveLogo,
+    alt: "Valve Logo",
+  },
+];
+
+export default footerItem;
